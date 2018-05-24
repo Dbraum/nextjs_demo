22d44aa21f0fc8902c94e7ed0779a220628bc399
bulong <bulong.wkp@dtyunxi.com>
2018-05-24 21:12:16 +0800

    删除没用代码

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogActions from "@material-ui/core/DialogActions";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import { withStyles } from "@material-ui/core/styles";
 import { withRoot } from "../src/hoc";
 import GoodsSwipe from "../src/components/googs/GoodsSwipe";
@@ -24,7 +16,6 @@ const styles = theme => ({
 @withStyles(styles)
 export default class Index extends React.Component {
   state = {
-    open: false,
     goods: {
       title: "美国伽力果（约680g/3个）",
       price: 2680,
@@ -37,28 +28,16 @@ export default class Index extends React.Component {
     }
   };
 
-  handleClose = () => {
-    this.setState({
-      open: false
-    });
-  };
-
-  handleClick = () => {
-    this.setState({
-      open: true
-    });
-  };
-
   render() {
     const { classes } = this.props;
-    const { open,goods={} } = this.state;
-    const { thumbs=[] } = goods;
+    const { goods = {} } = this.state;
+    const { thumbs = [] } = goods;
 
     return (
       <Page>
         <Body>
-          <GoodsSwipe imgs={thumbs}/>
-          <InsetDividers goods={goods}/>
+          <GoodsSwipe imgs={thumbs} />
+          <InsetDividers goods={goods} />
         </Body>
         <Footer>
           <CartAction />
